0cf5994b998e41f1b1b34d09613145b7db14dda9
Brownhill Udeh <brownhilludeh@gmail.com>
2023-08-23 17:49:23 +0100

    main

--- index.py
+++ index.py
@@ -1,31 +1,38 @@
-# Operators
-print (10//3)
-print (10/3)
-print (10+3)
-print (10-3)
-print (10*3)
-print (10**3)
-
-# examples of Variable Datatypes
-Float = 1.444535
-Integer = 4
-string = 'Uche'
-email = 'info@brownportal.com'
-
-# print (email)
-name = input('Enter name: ')
-nameFriend = input("Enter friend's name: ")
-
-# print(name)
-
-age = 25
-className = 'level 8'
-blood = 'A+'
-gene = 'AA'
-food = ['plantain', 'Eforiro', 'apple', 'mango', 'garri']
-
-phone = '08086787565'
-
-print ( f" Hi, Did you know that {name} and {nameFriend} like {food} ? He's {age} years old and she's {blood}. We both are {gene}" )
+# # Operators
+# print (10/3)
+# print (10//3)
+# print (10+3)
+# print (10-3)
+# print (10*3)
+# print (10**3)
 
 
+# # examples of Variable Datatypes
+# Float = 1.444535
+# Integer = 4
+# string = 'Uche'
+# email = 'info@brownportal.com'
+
+# # print (email)
+# name = input('Enter name: ')
+# nameFriend = input("Enter friend's name: ")
+
+# # print(name)
+
+# age = 25
+# className = 'level 8'
+# blood = 'A+'
+# gene = 'AA'
+# food = ['plantain', 'Eforiro', 'apple', 'mango', 'garri']
+
+# phone = '08086787565'
+
+# print ( f" Hi, Did you know that {name} and {nameFriend} like {food} ? He's {age} years old and she's {blood}. We both are {gene}" )
+age = 10
+
+if age < 10:
+    print ("Oga grow up!")
+elif age > 10:
+    print("Bro, go away ")
+else:
+    print('you can play the game')
